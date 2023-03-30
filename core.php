20cb5d80bdeabc45cca0ce0693a9c8e3f9dfe9a0
Volkan Sah <VolkanSah@users.noreply.github.com>
2023-03-30 14:30:18 +0200

    Update core.php

--- core.php
+++ core.php
@@ -1,14 +1,29 @@
 <?php
-/*
-Plugin Name: Facebook Engagement Metrics
-Description: Zeigt Facebook Engagement Metriken für veröffentlichte Links im WordPress Dashboard an.
-Version: 1.0
-Author: Volkan Sah Kücükbudak
-*/
-
+/**
+ * Plugin Name
+ *
+ * @package           Facebook Engagement Metrics
+ * @author            Volkan Kücükbudak
+ * @copyright         2023 WordPress-Webmaster.de
+ * @license           Privat
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Facebook Engagement Metrics
+ * Plugin URI:        https://wordPress-webmaster.de
+ * Description:       The Facebook Engagement Metrics WordPress Plugin displays engagement metrics such as likes, shares, and comments for published links on your WordPress dashboard. This tool is essential for social media and SEO marketing as it provides insights into the performance of your Facebook posts and allows you to optimize your content strategy accordingly. Use our plugin to monitor the impact of your social media marketing efforts, identify top-performing posts, and track engagement trends over time. Stay ahead of the competition and improve your online presence with our easy-to-use Facebook engagement metrics plugin for WordPress.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Your Name
+ * Author URI:        https://github.com/VolkanSah/WP-Facebook-Engagement-Metrics/
+ * Text Domain:       fb-em
+ * License:           GPL v2 or later
+ * License URI:       https://wordPress-webmaster.de/licenses/wordpress-global.txt
+ * Update URI:        https://github.com/VolkanSah/WP-Facebook-Engagement-Metrics/
+ */
 // Create Option Page
 function fb_engagement_metrics_options_page() {
-    add_options_page(
+        add_options_page(
         'Facebook Engagement Metrics',
         'Facebook Engagement Metrics',
         'manage_options',
@@ -16,19 +31,15 @@ function fb_engagement_metrics_options_page() {
         'fb_engagement_metrics_options_page_html'
     );
 }
-
 add_action('admin_menu', 'fb_engagement_metrics_options_page');
-
 // Create Option Page HTML
 function fb_engagement_metrics_options_page_html() {
     if (!current_user_can('manage_options')) {
         return;
     }
-
     if (isset($_GET['settings-updated'])) {
         add_settings_error('fb_engagement_metrics_messages', 'fb_engagement_metrics_message', 'Einstellungen gespeichert.', 'updated');
     }
-
     settings_errors('fb_engagement_metrics_messages');
     ?>
     <div class="wrap">
@@ -43,17 +54,14 @@ function fb_engagement_metrics_options_page_html() {
     </div>
     <?php
 }
-
 function fb_engagement_metrics_settings_init() {
     register_setting('fb_engagement_metrics', 'fb_engagement_metrics_options');
-
     add_settings_section(
         'fb_engagement_metrics_section',
         'Einstellungen',
         null,
         'fb-engagement-metrics'
     );
-
     add_settings_field(
         'fb_engagement_metrics_app_id',
         'App-ID',
@@ -61,7 +69,6 @@ function fb_engagement_metrics_settings_init() {
         'fb-engagement-metrics',
         'fb_engagement_metrics_section'
     );
-
     add_settings_field(
         'fb_engagement_metrics_app_secret',
         'App-Geheimcode',
@@ -70,23 +77,19 @@ function fb_engagement_metrics_settings_init() {
         'fb_engagement_metrics_section'
     );
 }
-
 add_action('admin_init', 'fb_engagement_metrics_settings_init');
-
 function fb_engagement_metrics_app_id_callback() {
     $options = get_option('fb_engagement_metrics_options');
     ?>
     <input type="text" name="fb_engagement_metrics_options[app_id]" value="<?= esc_attr($options['app_id']); ?>" size="50">
     <?php
 }
-
 function fb_engagement_metrics_app_secret_callback() {
     $options = get_option('fb_engagement_metrics_options');
     ?>
     <input type="text" name="fb_engagement_metrics_options[app_secret]" value="<?= esc_attr($options['app_secret']); ?>" size="50">
     <?php
 }
-
 function generate_facebook_access_token($app_id, $app_secret) {
     $token_url = "https://graph.facebook.com/oauth/access_token?client_id={$app_id}&client_secret={$app_secret}&grant_type=client_credentials";
     $response = file_get_contents($token_url);
@@ -95,10 +98,8 @@ function generate_facebook_access_token($app_id, $app_secret) {
 }
 function fetch_facebook_metrics($url, $access_token) {
     $request_url = "https://graph.facebook.com/v13.0/?id=" . urlencode($url) . "&fields=engagement&access_token=" . $access_token;
-
     $response = file_get_contents($request_url);
     $json = json_decode($response, true);
-
     $likes = $json['engagement']['reaction_count'];
     $shares = $json['engagement']['share_count'];
     $comments = $json['engagement']['comment_count'];
@@ -118,7 +119,6 @@ function get_published_links() {
         $query->the_post();
         $links[] = get_permalink();
     }
-
     wp_reset_postdata();
 
     return $links;
@@ -145,9 +145,7 @@ function display_facebook_metrics() {
     }
     echo "</table>";
 }
-
 function add_facebook_metrics_dashboard_widget() {
     wp_add_dashboard_widget('facebook_metrics_dashboard_widget', 'Facebook Engagement Metrics', 'display_facebook_metrics');
 }
-
 add_action('wp_dashboard_setup', 'add_facebook_metrics_dashboard_widget');
